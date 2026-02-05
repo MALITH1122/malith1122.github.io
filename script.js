0119802702b771af1d7a2553539c45b05db66392
MALITH1122 <116143453+MALITH1122@users.noreply.github.com>
2026-02-05 16:32:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2867
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 // script.js\
+const year = document.getElementById("year");\
+year.textContent = new Date().getFullYear();\
+\
+const themeBtn = document.getElementById("themeBtn");\
+themeBtn.addEventListener("click", () => \{\
+  document.body.classList.toggle("light");\
+\});\
+\
+const menuBtn = document.getElementById("menuBtn");\
+const navLinks = document.getElementById("navLinks");\
+\
+menuBtn.addEventListener("click", () => \{\
+  const open = navLinks.classList.toggle("open");\
+  menuBtn.setAttribute("aria-expanded", String(open));\
+\});\
+\
+navLinks.querySelectorAll("a").forEach(a => \{\
+  a.addEventListener("click", () => navLinks.classList.remove("open"));\
+\});}
